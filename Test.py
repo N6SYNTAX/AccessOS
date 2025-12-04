7463aee00567e632eb5ecd2ee9cf8cfd96a35d08
N6SYNTAX <127181966+N6SYNTAX@users.noreply.github.com>
2025-12-04 21:23:22 +1100

    Initial commit

---
+++ Test.py
@@ -0,0 +1,15 @@
+import requests
+
+
+ICPName = "test"
+Username = "test"
+Password = "test"
+APIURL = "api/v1/authentication/login" + ICPName
+
+def get_user(name):
+    pass
+
+UserName = "Installer"
+
+get_user(UserName)
+
