40ad9eef7c831f62e304730dc727a3c25a7ef5e9
N6SYNTAX <127181966+N6SYNTAX@users.noreply.github.com>
2025-12-13 10:20:46 +1100

    Front end

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("Hello");
+console.log("AccessOS");
+
+document.getElementById("p1").textContent = "Please Login To Your Inception";
+
+// Ternary Operator
+//age >= 18 ? "Youre an adult" : "Youre a minor";
+// Constant
+//const pi = 3.14;
+
+let hello = "hello";
+let ICPSerial;
+let Username;
+let Password;
+
+document.getElementById("ICPSubmit").onclick = function () {
+    ICPSerial = document.getElementById("ICPSerial").value;
+    APIUsername = document.getElementById("APIUsername").value;
+    APIPassword = document.getElementById("APIPassword").value;
+}
+
+
+
+if (ICPSerial = 123456) {
+
+}
